fb9e0356222cea7770e146e2a20cbb1faf57567c
Joshua Odedeyi <hollyray2003@gmail.com>
2025-05-23 15:54:52 +0100

    added rectanle_area

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+# This program calculates the area of a rectangle
+# Program written by Joshua Odedeyi
+# Date: 2023-10-04
+length = 10  # Length of the rectangle in meters
+width = 5  # Width of the rectangle in meters
+# Calculate the area using the formula: Area = Length * Width
+area = length * width  # Area in square meters
+print("The Area of the Rectangle is", area)
+# Output: The Area of the Rectangle is 50
