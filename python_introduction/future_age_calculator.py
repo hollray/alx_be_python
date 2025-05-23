aab42b5ef82fd38c80fb1510e33c991e27486af5
Joshua Odedeyi <hollyray2003@gmail.com>
2025-05-23 16:33:01 +0100

    adde future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+# This program calculates the age of a person"
+# Program written by Joshua Odedeyi
+# Date: 2025-May-23
+age = int(input("How old are you?"))
+current_year = 2023
+# This gives the user the option to chose the year they want to check
+future_year = int(input("Enter the year you want to know your age in: "))
+# This calculates the future age by subtracting the current year from the future year and adding the current age
+future_age = future_year - current_year + age
+# This prints the future age of the user
+print("in", future_year, "you will be", future_age, "years old")
