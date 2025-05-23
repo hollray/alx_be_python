b9ee5ebf44b5136fd822c7e5585e2eaaa06dae56
Joshua Odedeyi <hollyray2003@gmail.com>
2025-05-23 16:18:41 +0100

    added hours_to_minutes.py project

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+# This Program converts hours to seconds
+# Program written by Joshua Odedeyi
+# Date: 23-May-2025
+hours = 2  # Number of hours
+seconds = hours * 3600  # Convert hours to seconds (1 hour = 3600 seconds)
+print(hours, "hours is ", seconds, "seconds")
