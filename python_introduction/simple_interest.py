23a547ba30abff28840a246ecd8df84162cbbb7a
Joshua Odedeyi <hollyray2003@gmail.com>
2025-05-23 15:20:30 +0100

    added simple_interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# This program calculates simple interest
+# Program written by Joshua Odedeyi
+# Date: 2023-10-04
+principal_amount = 1000  # Principal amount in dollars
+annual_rate = 0.05  # Annual interest rate (5%)
+time = 3  # Expressed in Years
+# Calculate simple interest using the formula: I = P * r * t
+interest = principal_amount * annual_rate * time  # Interest earned in dollars
+print("The Simple Interest is: $", interest)
