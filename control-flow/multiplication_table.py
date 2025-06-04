db40f3af5fcbf7ec24bb423b8f1b62cb6607bf38
Joshua Odedeyi <hollyray2003@gmail.com>
2025-06-04 16:00:39 +0100

    correction to multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -4,5 +4,6 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 for current_number in range(1, 11):
-    product = number * current_number
-    print(f"{number} * {current_number} = {product}")
+    y = current_number
+    z = number * y
+    print(f"{number} * {y} = {z}")
