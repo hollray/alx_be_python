852864e42cde160b42e0f301bd0cd4e665624f75
Joshua Odedeyi <hollyray2003@gmail.com>
2025-06-04 16:18:46 +0100

    new line introduced

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,10 @@
 # This script will ask the user to enter a number,
 # It then use a for loop to print the multiplication table for that number from 1 to 10.
 
-number = int(input("Enter a number to see its multiplication table: "))
+# Program written by Joshua Odedeyi
+# Date: 2025-June-04
+
+number = int(input("Enter a number to see its multiplication table: \n"))
 
 for y in range(1, 11):
     x = number
