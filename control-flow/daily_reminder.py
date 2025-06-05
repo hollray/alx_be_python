d5d00bb225a22d549b35810078caf8734729159e
Joshua Odedeyi <hollyray2003@gmail.com>
2025-06-05 16:17:04 +0100

    removed .lower( ) function

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -17,10 +17,10 @@ match priority:
     case _:
         print("Unknown priority.")
 # and a match-case structure to categorize tasks.
-if time_bound.lower() == "yes":
+if time_bound == "yes":
     print(
         f"Reminder: {task} is a {message} priority task that requires immediate attention today!")
-elif time_bound.lower() == "no":
+elif time_bound == "no":
     print(
         f"Note: {task} is a {message} priority task. Consider completing it when you have free time.")
 else:
